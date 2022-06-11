7236463434ec7d8121d48ea478e02ba9da6fb2c3
Javier Fuentes <javierfuentes545@gmail.com>
2022-06-10 23:50:34 -0500

    [add] animate card, configure next js for svg

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const optimizedImages = require("next-optimized-images");
 const nextConfiguration = {
   images: {
     disableStaticImages: true,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   target: "serverless", //will output independent pages that don't require a monolithic server. It's only compatible with next start or Serverless deployment platforms (like ZEIT Now) — you cannot use the custom server API.
 };
--- src/sections/banner.js
+++ src/sections/banner.js
@@ -10,30 +10,30 @@ import BannerBG from "assets/bannerBg.png";
 import BannerSVG from "assets/background.svg";
 import BannerThumb from "assets/banner-thumb.png";
 
-import client1 from "assets/sponsor/paypal.svg";
-import client2 from "assets/sponsor/google.svg";
-import client3 from "assets/sponsor/dropbox.svg";
+// import client1 from "assets/sponsor/paypal.svg";
+// import client2 from "assets/sponsor/google.svg";
+// import client3 from "assets/sponsor/dropbox.svg";
 
-const data = [
-  {
-    id: 1,
-    path: "#",
-    image: client1,
-    title: "paypal",
-  },
-  {
-    id: 2,
-    path: "#",
-    image: client2,
-    title: "google",
-  },
-  {
-    id: 3,
-    path: "#",
-    image: client3,
-    title: "dropbox",
-  },
-];
+// const data = [
+//   {
+//     id: 1,
+//     path: "#",
+//     image: client1,
+//     title: "paypal",
+//   },
+//   {
+//     id: 2,
+//     path: "#",
+//     image: client2,
+//     title: "google",
+//   },
+//   {
+//     id: 3,
+//     path: "#",
+//     image: client3,
+//     title: "dropbox",
+//   },
+// ];
 
 export default function Banner() {
   const [videoOpen, setVideoOpen] = useState(false);
